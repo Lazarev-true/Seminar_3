ab73cb7db0ef6d9841784e37354c85d8259185e5
Lazarev Konstantin <klik@example.com>
2022-08-28 19:26:41 +0300

    Куб чисел

---
+++ Task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и помещает в массив таблицу кубов чисел от 1 до N. 
+// Полученный массив вывести на экран.
+
+Console.WriteLine("Введите число N");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[N];
+
+for (int i = 0; i < N; i++)
+{
+   array[i] = (int)Math.Pow((i + 1), 3);
+
+   Console.WriteLine($"{i + 1}^3 = {array[i]}");
+}
+
